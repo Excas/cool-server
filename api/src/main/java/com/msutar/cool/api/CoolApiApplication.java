ae2098a6e2187b1402e7f9888435104112b00917
xuchen <an759520>
2022-03-25 15:14:02 +0800

    join api project

---
+++ api/src/main/java/com/msutar/cool/api/CoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.msutar.cool.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolApiApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/msutar/cool/api/CoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msutar.cool.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
