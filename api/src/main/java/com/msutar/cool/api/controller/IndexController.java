ae41ca8a7a189d2c54f7367aeb3595c09e66459c
Excas <an759520>
2022-03-25 15:28:32 +0800

    make a controller to test my commit username

---
+++ api/src/main/java/com/msutar/cool/api/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.msutar.cool.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("index")
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public String index () {
+        return "hello";
+    }
+}
