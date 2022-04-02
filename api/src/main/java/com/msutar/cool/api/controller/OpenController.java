aa86b868e66a5fec173347a68591d791ed580b22
Excas <932233308@qq.com>
2022-04-02 15:57:34 +0800

    code valid

--- api/src/main/java/com/msutar/cool/api/controller/OpenController.java
+++ api/src/main/java/com/msutar/cool/api/controller/OpenController.java
@@ -1,7 +1,10 @@
 package com.msutar.cool.api.controller;
 
 import com.msutar.cool.api.common.annotation.Limit;
+import com.msutar.cool.api.common.entity.CommonConstant;
+import com.msutar.cool.api.common.exception.CommonException;
 import com.msutar.cool.api.common.service.CaptchaService;
+import com.msutar.cool.api.common.service.RedisService;
 import com.msutar.cool.api.dto.LoginDTO;
 import com.msutar.cool.api.vo.CaptchaVO;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +18,7 @@ import javax.validation.Valid;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class OpenController {
     private final CaptchaService captchaService;
-
+    private final RedisService redisService;
 
     @GetMapping("captcha")
     @Limit(key = "get_captcha", period = 60, count = 10, name = "获取验证码", prefix = "limit")
@@ -23,9 +26,12 @@ public class OpenController {
         return captchaService.create();
     }
 
-
     @PostMapping("login")
     public String login(@Valid @RequestBody LoginDTO dto) {
+        // 校验验证码
+        String code = (String) redisService.get(CommonConstant.VALIDATE_CODE_PREFIX + dto.getCaptchaId());
+        if (code == null || !code.equals(dto.getVerifyCode())) throw new CommonException("验证码错误");
+        // TODO 校验账号密码
 
         return null;
     }
